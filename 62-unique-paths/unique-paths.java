44074179acbe4302ac7e83c6896cb92d5f55e18b
sameergoru18 <sameergoru18@gmail.com>
2026-09-03 22:10:39 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (65.78%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int paths(int i,int j,int[][] dp){
+        if(i<0 || j<0) return 0;
+        if(i==0 && j==0){
+            return 1;
+        }
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        int left = paths(i,j-1,dp);
+        int up = paths(i-1,j,dp);
+
+        dp[i][j]=left+up;
+        return dp[i][j];
+
+
+
+
+    }
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int i=0;i<m;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        int arr[]=new int[1];
+        return paths(m-1,n-1,dp);
+        
+    }
+}
